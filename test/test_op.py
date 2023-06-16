286dd4b1292bd06f808fdf7def35910f360e8288
wangfengsheng1999 <wangfengsheng1999@gmail.com>
2023-06-16 11:38:56 +0000

    add test case

--- test/test_op.py
+++ test/test_op.py
@@ -1,10 +1,20 @@
 import unittest
 
 import sys
-sys.path.append('../python')
+sys.path.append('./python')
 import TransFTrain as train
  
 x = train.Tensor([1,2,3], dtype="int8")
 y = train.Tensor(x)
 
-print(x)
+def add_numbers(x, y):
+    return x + y
+
+class TestSummation(unittest.TestCase):
+    def test_addition(self):
+        self.assertEqual(add_numbers(2, 3), 5)
+        self.assertEqual(add_numbers(-1, 1), 0)
+        self.assertEqual(add_numbers(0, 0), 0)
+
+if __name__ == '__main__':
+    unittest.main()
